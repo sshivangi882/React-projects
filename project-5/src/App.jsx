624531c604815aa484df238a976292cbc36dec52
Shivangi Sharma <sshivangi882@gmail.com>
2024-10-08 14:06:14 +0530

    .

--- project-5/src/App.jsx
+++ project-5/src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
 import './index.css'
-import Navbar from '../components/Navbar'
+import Navbar from './components/Navbar'
+import {FiSearch} from 'react-icons/fi'
+import {AiFillPlusCircle} from 'react-icons/ai'
 
 function App() {
 
-  return <div>
-    <Navbar />
+  return (
+    <div className='max-w-[370px] mx-auto px-4'>
+      <Navbar />
+     <div className='flex gap-2'>
+     <div className='flex relative flex-grow items-center' >
+        <FiSearch className='text-white text-xl flex-grow absolute mx-1' />
+        <input type='text' className='border bg-transparent px-7 border-white flex-grow rounded-md h-[40px]' placeholder='search contact details'/>
+      </div>
+      <div>
+        <AiFillPlusCircle className='text-white text-4xl gap-4  cursor-pointer' />
+      </div>
+     </div>
   </div>
+  )
 }
 
 export default App
--- project-5/src/components/Navbar.jsx
+++ project-5/src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 
 const Navbar = () => {
   return (
-    <div className="h-[60px] bg-white m-4 rounded-lg">
-      <div className="flex">
-        
-      </div>
+    <div className="h-[60px] gap-2 bg-white my-4 rounded-lg flex items-center justify-center text-xl  font-medium">
+        <img src="/firebase.svg" />
+        <h1>Firebase Contact App</h1>
     </div>
   )
 }
